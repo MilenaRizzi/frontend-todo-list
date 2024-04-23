7461e9306e117b4df586d3a8da26deffe6d32758
Your Name <milenaclararizzi@gmail.com>
2024-04-23 15:37:24 -0300

    Update useTaskDataMutate.ts

--- src/components/hooks/useTaskDataMutate.ts
+++ src/components/hooks/useTaskDataMutate.ts
@@ -1,13 +1,13 @@
-import axios, { AxiosPromise } from "axios"
+import axios, { AxiosPromise } from "axios";
 import { TaskData } from "../interface/TaskData";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-const API_URL = 'http://localhost:8080';
+const API_URL = "http://localhost:8080";
 
 const postData = async (data: TaskData): AxiosPromise<TaskData> => {
-  const response = axios.post(API_URL + '/tasks', data);
+  const response = axios.post(API_URL + "/tasks", data);
   return response;
-}
+};
 
 export function useTaskDataMutate() {
   const queryClient = useQueryClient();
@@ -15,9 +15,9 @@ export function useTaskDataMutate() {
     mutationFn: postData,
     retry: 2,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['tasks-data'] });
-    }
-  })
+      queryClient.invalidateQueries({ queryKey: ["tasks-data"] });
+    },
+  });
 
   return mutate;
 }
