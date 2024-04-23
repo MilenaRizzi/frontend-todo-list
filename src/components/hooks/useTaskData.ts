c184574ac75e0b1d23a7abeabeefb6cad56c8656
Your Name <milenaclararizzi@gmail.com>
2024-04-23 15:37:18 -0300

    Update useTaskData.ts

--- src/components/hooks/useTaskData.ts
+++ src/components/hooks/useTaskData.ts
@@ -1,23 +1,23 @@
-import axios, { AxiosPromise } from "axios"
+import axios, { AxiosPromise } from "axios";
 import { TaskData } from "../interface/TaskData";
 import { useQuery } from "@tanstack/react-query";
 
-const API_URL = 'http://localhost:8080';
+const API_URL = "http://localhost:8080";
 
 const fetchData = async (): AxiosPromise<TaskData[]> => {
-  const response = await axios.get(API_URL + '/tasks');
+  const response = await axios.get(API_URL + "/tasks");
   return response;
-}
+};
 
 export function useTaskData() {
   const query = useQuery({
     queryFn: fetchData,
-    queryKey: ['tasks-data'],
-    retry: 2
-  })
+    queryKey: ["tasks-data"],
+    retry: 2,
+  });
 
   return {
     ...query,
     data: query.data?.data,
-  }
+  };
 }
